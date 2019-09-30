7e9b6fe52bf617d5dd1593e673685c1341124a7a
xiaoqieZ <kll.zhou@qq.com>
2019-09-30 12:00:22 +0800

    回访记录

--- src/PublicNumber/Publicfore/TransactionRecord/Purchase.vue
+++ src/PublicNumber/Publicfore/TransactionRecord/Purchase.vue
@@ -5,16 +5,7 @@
                 <mt-button icon="back">返回</mt-button>
             </router-link>
         </mt-header>
-        <div>
-            <p>尊敬的投资者<span>- -</span>,您好！</p>
-            <p>以下内容为您购买的产品：<span>- -</span>的回访确认问卷，请您如实作答：</p>
-        </div>
-        <div v-for="item in radioData" :key="item.id">
-            <p>{{item.title}}</p>
-            <div v-for="open in item" :key="open.id">
-                <el-radio v-model="radio" :label="open.score">{{open.content}}</el-radio>
-            </div>           
-        </div>
+        
     </div>
 </template>
 
@@ -29,9 +20,7 @@ export default {
     },
     methods:{
         getRadio(){
-            ajax.authGet.bind(this)('/api/Information/Present/Product/VisitBack/Questionnaire?visitId='+1,res=>{
-                this.radioData=res.data.data
-            })
+            
         }
     },
     mounted(){
